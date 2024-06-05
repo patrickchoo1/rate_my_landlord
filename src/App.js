0afb6e845bc37c4f5e141757e2884d1ad4b2f414
pkc48 <pkc48@cornell.edu>
2024-06-05 15:03:28 -0400

    Created JS and CSS for search bar

--- src/App.js
+++ src/App.js
@@ -8,15 +8,10 @@ function App() {
     <section className = 'hero'>
       <div className = 'content'>
         <h1>Rate your landlords</h1>
-        <p>
-          Review your landlords. Rate their properties.
-        </p>
-        <div className="search">
-          <input
-          type="text"
-          placeholder="Search..."
-          />
-      </div>
+        <p> Review your landlords. Rate their properties. </p>
+        <div className = 'search-wrapper'>
+          <input class = 'search' input type = 'text' placeholder = "Enter Landlord's Name"></input>
+        </div>
       </div>
     </section>
   );
