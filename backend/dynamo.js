4843bf58a706a0adedb086a715a8af5b64d7c739
pkc48 <pkc48@cornell.edu>
2024-06-26 14:08:14 -0400

    Started creating json for adding new landlords

--- backend/dynamo.js
+++ backend/dynamo.js
@@ -10,6 +10,29 @@ AWS.config.update({
 const dynamoClient = new AWS.DynamoDB.DocumentClient();
 const TABLE_NAME = "rml_landlordinfo";
 
+const createDistribution = () => {
+    return [
+        {rating: 5, count: 0},
+        {rating: 4, count: 0},
+        {rating: 3, count: 0}, 
+        {rating: 2, count: 0}, 
+        {rating: 1, count: 0}
+    ]
+    }
+
+const updateDistribution = (distribution, rating) => {
+    return {
+        pass
+    }
+}
+
+const createlandlordJSON = (landlord_name, property_name, distribution, reviews, total_rating) => {
+    return {
+        "landlord_name": landlord_name,
+        "property_name": property_name,
+    }
+}
+
 const getLandlord = async () => {
     const params = {
         TableName: TABLE_NAME
